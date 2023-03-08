83f28ec8a8fd3df33522909cd3cd2f6f79c52740
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-03-08 08:12:43 +0700

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
