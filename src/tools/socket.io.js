be7ba0e7ff3db3585c71f514dede92052a216695
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-03-08 11:40:27 +0700

    edit src/tools/socket.io.js

--- src/tools/socket.io.js
+++ src/tools/socket.io.js
@@ -1,3 +1,5 @@
 import io from 'socket.io-client';
-const socket = io.connect('http://localhost:5000');
+const CHAT_SERVER_URL =
+    process.env.REACT_APP_CHAT_SERVER_URL || 'http://localhost:5000';
+const socket = io.connect(CHAT_SERVER_URL);
 export default socket;
